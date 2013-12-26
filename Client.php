8c506e5fee29581f946c60c7762dc11cda88a9f0
Andor Bibók <bibok.andor@gmail.com>
2013-12-27 00:17:34 +0100

    new functions and added error handling functions: excludeFile(), includeFile(), includeDir()

--- Client.php
+++ Client.php
@@ -34,18 +34,82 @@ class Client /*extends Deploy*/ {
 	public function writeDir() {
 		$chop = $this->projectPath ? strlen($this->projectPath) + 1 : 0;
 		
+		sort($this->files);
 		$file = fopen('files.txt', 'w');
+		if( !$file ) {
+			throw new Exception("Failed to open file: files.txt for writing!");
+		}
 		foreach( $this->files as $f ) {
-			fwrite($file, md5_file($f) .' '. substr($f, $chop) . "\n");
+			fwrite($file, substr($f, $chop) . "\n");
 		}
 		fclose($file);
 		
+		sort($this->directories);
 		$file = fopen('directories.txt', 'w');
+		if( !$file ) {
+			throw new Exception("Failed to open directories: files.txt for writing!");
+		}
 		foreach( $this->directories as $d ) {
 			fwrite($file, substr($d, $chop) . "\n");
 		}
 		fclose($file);
 
+	}
+	
+	public function excludeFile( $file, $withPath = false ) {
+		$chop = $this->projectPath ? strlen($this->projectPath) + 1 : 0;
+		$newfiles	= array();
+		
+		foreach( $this->files as $f ) {
+			if( $withPath ) {
+				$ret = preg_match($file, substr($f, $chop));
+				if( $ret === 0 ) {
+					$newfiles[] = $f;
+				} else if( $ret === false ) {
+					throw new Exception("Failed to parse filename for excluding file! (1)");
+				}
+			} else {
+				$ret = preg_match("/.*[\/]([^\/]*)$/", substr($f, $chop), $m);
+				if( $ret ) {
+					$ret = preg_match($file, $m[1]);
+					if( $ret === 0 ) {
+						$newfiles[] = $f;
+					} else if( $ret === false ) {
+						throw new Exception("Failed to parse filename for excluding file! (2)");
+					}
+				} else if( $ret === false ) {
+					throw new Exception("Failed to parse filename to get dirname for excluding file!");
+				} else {
+					$ret = preg_match($file, substr($f, $chop));
+					if( $ret === 0 ) {
+						$newfiles[] = $f;
+					} else if( $ret === false ) {
+						throw new Exception("Failed to parse filename for excluding file! (3)");
+					}
+				}
+			}
+		}
+		
+		$this->files = $newfiles;
+	}
+	
+	public function includeFile( $file ) {
+		if( $this->projectPath ) {
+			$file = $this->projectPath .'/'. $file;
+		}
+		
+		if( !file_exists($file) ) {
+			throw new Exception("Failed to include file: $file (not found)");
+		}
+		
+		$ret = preg_match("/(.*)[\/][^\/]*$/", $file, $m);
+		if( $ret ) {
+			$this->includeDir($m[1], false);
+		} else if( $ret === false ) {
+			throw new Exception("Failed to parse filename $file for including!");
+		}
+		$this->files[] = $file;
+		$this->files = array_unique( $this->files );
 	}
 	
 	public function excludeDir( $dir ) {
@@ -58,7 +122,7 @@ class Client /*extends Deploy*/ {
 			if( $ret === 0 ) {
 				$newdirs[] = $d;
 			} else if( $ret === false ) {
-				//hiba
+				throw new Exception("Failed to parse dirname for excluding directory!");
 			}
 		}
 		foreach( $this->files as $f ) {
@@ -68,10 +132,12 @@ class Client /*extends Deploy*/ {
 				if( $ret === 0 ) {
 					$newfiles[] = $f;
 				} else if( $ret === false ) {
-					//hiba
+					throw new Exception("Failed to parse filename for excluding directory!");
 				}
 			} else if( $ret === false ) {
-				//hiba
+				throw new Exception("Failed to parse filename to get dirname for excluding directory!");
+			} else {
+				$newfiles[] = $f;
 			}
 			
 		}
@@ -80,11 +146,19 @@ class Client /*extends Deploy*/ {
 		$this->directories = $newdirs;
 	}
 	
+	public function includeDir( $dir, $prefix = true ) {
+		if( $prefix && $this->projectPath ) {
+			$dir = $this->projectPath .'/'. $dir;
+		}
+		$this->directories[] = $dir;
+		$this->directories = array_unique( $this->directories );
+	}
+	
 	private function zipFiles( $name = 'deploy.zip', $path = '' ) {
 		$zip = new ZipArchive;
 		$chop = $this->projectPath ? strlen($this->projectPath) + 1 : 0;
 		
-		$ret = $zip->open($path.$name, ZipArchive::EXCL );
+		$ret = $zip->open($path.$name, ZipArchive::CREATE);
 		if( $ret === true ) {
 			foreach( $this->files as $f ) {
 				if( !$zip->addFile($f, 'src/'.substr($f,$chop)) ) {
@@ -93,6 +167,13 @@ class Client /*extends Deploy*/ {
 			}
 		} else {
 			throw new Exception("Can't create archive: $path$name! Zip error code: $ret");
+		}
+		
+		if( !$zip->addFile('files.txt') ) {
+			throw new Exception("Can't add file: files.txt to archive!");
+		}
+		if( !$zip->addFile('directories.txt') ) {
+			throw new Exception("Can't add file: directories.txt to archive!");
 		}
 		
 		if( !$zip->close() ) {
@@ -104,6 +185,7 @@ class Client /*extends Deploy*/ {
 		$this->projectPath ? $this->readDir($this->projectPath) : $this->readDir('.');
 		$this->preDeployScript();
 		$this->writeDir();
+		
 		
 		$this->zipFiles();
 		
