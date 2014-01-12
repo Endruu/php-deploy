2bd1659087cbb516a5458e6efa465d433f5bdbc7
Andor Bibók <bibok.andor@gmail.com>
2014-01-12 19:18:55 +0100

    setters modified

--- protected/Deploy.php
+++ protected/Deploy.php
@@ -4,6 +4,7 @@ require_once 'DeployClient.php';
 require_once 'DeployServer.php';
 
 class Deploy {
+	public $workDir = null;
 
 	private function getOptions( $iniPath ) {
 		$optOuter = parse_ini_file($iniPath, true);
@@ -18,35 +19,35 @@ class Deploy {
 		return array_merge($optInner, $optOuter);
 	}
 
-	public function deployClient( $ini ) {
+	public function deployClient( $ini, $workDir ) {
 		$base = new DeployClient;					// create the basic client class
 		
-		$base->createWorkingDirectory();			// create working directory
+		$wd = $base->setWorkPath($workDir);			// create working directory
 		$opt = $this->getOptions($ini);				// read options
 		
 		require_once($opt['client']['script']);		// include deploying script
 		$deployer = new $opt['client']['class'];	// create the deploying class
-		$deployer->init($this, $opt);				// initialize deploying class
+		$deployer->init($base, $opt);				// initialize deploying class
 		
 		$deployer->deploy();						// deploy
 		
 		$base->zipFiles();							// zip the created files
-		
+		$this->workDir = $wd;
 	}
 	
-	public function deployServer( $zip ) {
+	public function deployServer( $zip, $workDir ) {
 		$base = new DeployServer;					// create the basic server class
 		
-		$wd = $base->createWorkingDirectory();		// create working directory
+		$wd = $base->setWorkPath($workDir);			// create working directory
 		$base->unzipFiles($zip);					// unzip the deploying files
 		$opt = $this->getOptions($wd.'deploy.ini');	// read options
 		
 		require_once($opt['server']['script']);		// include deploying script
 		$deployer = new $opt['server']['class'];	// create the deploying class
-		$deployer->init($this, $opt);				// initialize deploying class
+		$deployer->init($base, $opt);				// initialize deploying class
 		
 		$deployer->deploy();						// deploy
-		
+		$this->workDir = $wd;
 	}
 	
 }
