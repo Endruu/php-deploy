61f0f41f4309aed9a431770e325b213344f3c225
Andor Bibók <bibok.andor@gmail.com>
2014-01-16 00:59:39 +0100

    started writing frontend

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-require_once 'protected/ClientDeployScripts.php';
-require_once 'protected/DeployServer.php';
+<head>
+	<title>Php-Deployer</title>
+</head>
+
+<body>
+
+<?php require_once 'protected/server.php'; 
+
+/*require_once 'protected/Deploy.php';
 
 try {
-//$d = new ClientDeployScripts;
-$d = new DeployServer;
-$d->projectPath = '../pannonia';
-$d->deploy();
+
+$d = new Deploy;
+echo "Client:<br />\n";
+$d->deployClient('projects/default/deploy.ini');
+sleep(1);
+echo "<br />\n";
+echo "Server:<br />\n";
+$d->deployServer('deploy.zip', 'protected/work/deploy');//$d->workDir.'deploy.zip');
+
 } catch( Exception $e ) {
 	echo $e->getMessage();
-}
+}*/
+
+?>
+
+</body>
+</html>
---
+++ protected/server.php
@@ -0,0 +1,83 @@
+<?php
+	
+	if( isset($_GET['a']) ) {
+		switch( $_GET['a'] ) {
+			case 'upload'	:
+				aUpload();
+				break;
+			case 'deploy'	:
+				aDeploy();
+				break;
+			default :
+				aError();
+		}
+	} else {
+		rIndex();
+	}
+
+
+function rIndex($post = null) {
+?>
+	<form action="index.php?a=upload" method="post" enctype="multipart/form-data">
+		<label for="file">Deploy file:</label>
+		<input type="file" name="file" id="file"><br />
+		<input type="submit" name="submit" value="Submit">
+	</form>
+<?php
+}
+
+function rUpload($post = null) {
+?>
+	<form action="index.php?a=upload" method="post" enctype="multipart/form-data">
+		<label for="file">Deploy file:</label>
+		<input type="file" name="file" id="file"><br />
+		<input type="submit" name="submit" value="Submit">
+	</form>
+<?php
+}
+
+function aUpload() {
+	$maxSize	= 25 * 1024 *1024;	//25MB
+
+	$error = '';
+	$arr = explode('.', $_FILES['file']['name']);
+	$ext = array_pop($arr);
+	
+	if( $_FILES['file']['error'] > 0 ) {
+		$error = "file:$error";
+	} else {
+		if( $ext == 'zip' || $ext == 'dep' ) {
+			if( $_FILES['file']['size'] < $maxSize ) {
+				$name = saveFile($_FILES['file']['tmp_name'], $ext);
+				//log!
+				echo $name;
+			} else {
+				$error = 'size';
+			}
+		} else {
+			$error = 'ext';
+		}
+	}
+	echo "<br />$error";
+}
+
+function aDeploy() {}
+
+function aError() {}
+
+function saveFile($tmpname, $ext, $path = 'protected/work/upload') {
+	$maxLength	= 40;
+	$newname = substr(sha1_file($tmpname), 0, $maxLength) .'.'. $ext;
+	
+	if( !is_dir($path) ) {
+		if( !mkdir( $path, 0777, true ) ) {
+			throw Exception("Can't create upload dir!");
+		}
+	}
+	
+	if( !file_exists($path.'/'.$newname) ) {
+		move_uploaded_file($tmpname, $path.'/'.$newname);
+	}
+	
+	return $newname;
+}
